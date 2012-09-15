c4ab2e77344bc6fd0664ddb1a3ca8c1e766546f3
ajlopez <webmaster@ajlopez.com>
2012-09-15 13:52:38 -0300

    Game, World, first tests

--- test/game.js
+++ test/game.js
@@ -2,6 +2,29 @@
 var gameserver = require('../'),
     assert = require('assert');
 
+// Create Game
+    
 var game = new gameserver.Game();
 
 assert.ok(game);
+
+// Create Default World
+
+game.createWorld(10, 10);
+
+// Get Default World
+
+var world = game.getWorld();
+
+assert.ok(world);
+assert.equal('default', world.name);
+
+// Get Default World by Name
+
+var defaultworld = game.getWorld('default');
+assert.equal(world, defaultworld);
+
+// Get Unknown World
+
+var unknown = game.getWorld('unknown');
+assert.equal(null, unknown);
